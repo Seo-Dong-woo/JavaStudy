95f96f0841fcaced44d8a96decaf48198424b6bf
SIST <SIST@DESKTOP-5G69MRI>
2023-09-25 13:52:13 +0900

    2023-09-25

--- Chapter04_배열/src/문제_4.java
+++ Chapter04_배열/src/문제_4.java
@@ -4,20 +4,13 @@ public class 문제_4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan=new Scanner(System.in);
-		System.out.print("정수 입력: ");
-		int[] arr=new int[10];
-		int total=0;
+		System.out.print("문자 입력: ");
+		char[] arr=new char[10];
 		for(int i=0;i<arr.length;i++)
 		{
-			arr[i]=scan.nextInt();
+			arr[i]=scan.next().charAt(0);
 		}
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.print(arr[i] + " ");
-			total=total+arr[i];
-			
-		}
-		System.out.println("\n평균: " + total/3.0);
+		System.out.println(arr);
 
 	}
 

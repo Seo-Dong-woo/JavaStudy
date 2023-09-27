84fa0e6358ab3ef0972da832a8fe97935caa181c
SIST <SIST@DESKTOP-5G69MRI>
2023-09-27 10:25:03 +0900

    2023-09-27

--- Chapter04_배열_4/src/배열_4.java
+++ Chapter04_배열_4/src/배열_4.java
@@ -5,7 +5,7 @@ public class 배열_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char[] arr=new char[10];
+		char[] arr=new char[5];
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=(char)((Math.random()*26)+65);
@@ -15,7 +15,7 @@ public class 배열_4 {
 		
 		for(int i=0;i<arr.length-1;i++)
 		{
-			for(int j=i+1;j<arr.length;j++)
+			for(int j=i+1;j<arr.length-1;j++)
 			{
 				if(arr[i]>arr[j])
 				{

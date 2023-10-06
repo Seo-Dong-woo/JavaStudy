6357b3126032f1d09c8459d272a58d0ace91f195
SIST <SIST@DESKTOP-5G69MRI>
2023-10-06 17:45:41 +0900

    2023-10-06

---
+++ Chapter04_메소드_4/src/메소드_3.java
@@ -0,0 +1,93 @@
+// 숫자 야구 게임
+import java.util.Arrays;
+import java.util.Scanner;
+/*
+ *   1. 컴퓨터 임의로 난수 발생 => []
+ *   2. 사용자 입력
+ *   3. 힌트
+ *   4. 종료여부
+ *   5. 조립
+ */
+
+public class 메소드_3 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 컴퓨터 임의로 난수 발생
+		int[] com=new int[3];
+		for(int i=0;i<com.length;i++)
+		{
+			com[i]=(int)(Math.random()*9)+1;
+			
+			for(int j=0;j<i;j++) // 중복 여부 확인하고 중복이면 다시 발생
+			{
+				if(com[i]==com[j])
+				{
+					i--;
+					break;
+				}
+			}
+		}
+		// System.out.println(Arrays.toString(com));
+		
+		// 사용자 입력
+		int[] user=new int[3];
+		Scanner scan=new Scanner(System.in);
+		while(true)
+		{
+			System.out.print("세자리 정수 입력: ");
+			int input=scan.nextInt();
+			
+			if(input<100 || input>999)
+			{
+				System.out.println("잘못된 입력입니다");
+				continue; // => for(증가식으로 이동), while(조건식으로 이동)
+				          // 처음부터 실행 => while
+				          // 제외 => for
+			}
+			
+			// 입력된 값을 배열에 저장
+			user[0]=input/100;
+			user[1]=(input%100)/10;
+			user[2]=input%10;
+			
+			if(user[0]==user[1] || user[1]==user[2] || user[0]==user[2])
+			{
+				System.out.println("같은 수는 사용할 수 없습니다");
+				continue;
+			}
+			
+			if(user[0]==0 || user[1]==0 || user[2]== 0)
+			{
+				System.out.println("0을 사용할 수 없습니다");
+				continue;
+			}
+
+			// hint 제공
+			int s=0, b=0;
+			for(int i=0;i<3;i++) // com
+			{
+				for(int j=0;j<3;j++) // user
+				{
+					if(com[i]==user[j]) // 같은 숫자 확인
+					{
+						if(i==j)
+							s++; // 같은 자리 숫자
+						else 
+							b++; // 다른 자리 숫자
+					}
+				}
+			}
+			// 힌트 출력
+			System.out.printf("Input Number: %d, Result: %dS-%dB\n", input, s, b);
+			
+			// 종료 여부 확인
+			if(s==3)
+			{
+				System.out.println("Game Over");
+				break; // 종료
+			}
+		}
+	}
+
+}
---
+++ Chapter04_메소드_4/src/메소드_매개변수전송법4.java
@@ -0,0 +1,21 @@
+
+public class 메소드_매개변수전송법4 {
+	static void swap(int a, int b) // 새로운 메모리 생성
+	{
+		System.out.println("변경 전 => swap:a=" + a + ", b=" + b); // 10, 20
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("변경 후 => swap:a=" + a + ", b=" + b); // 20, 10
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		// 기본형 => 복사(값) => Call By Value
+		System.out.println("변경 전 => main:a=" + a + ", b=" + b); // 10, 20
+		swap(a, b);
+		System.out.println("변경 후 => main:a=" + a + ", b=" + b); // 10, 20
+	}
+
+}

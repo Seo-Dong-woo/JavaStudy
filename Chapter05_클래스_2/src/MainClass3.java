1b8efbe0223e173436bf1216d3f834224814b049
SIST <SIST@DESKTOP-5G69MRI>
2023-10-10 17:48:44 +0900

    2023-10-10

---
+++ Chapter05_클래스_2/src/MainClass3.java
@@ -0,0 +1,28 @@
+// 영화 정보
+// 프로그램의 기본 => 묶는것 => 한 개의 이름으로 사용
+// 사용자 정의 데이터형
+// 자바에서 지원한 경우
+// 폴더가 다른 경우 접근 불가
+// default
+class Movie
+{
+	int movieId; // 고유번호 => 상세보기, 찾기
+	String poster, nameKr, nameEn;
+	String regdate;
+	String genre;
+	String nation;
+	String grade;
+	String time;
+	double score;
+	int showUser;
+	int boxOffice;
+	int startDay;
+}
+public class MainClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter05_클래스_2/src/MainClass5.java
@@ -0,0 +1,94 @@
+class Movie
+{
+	int movie_no;
+	String poster;
+	String name;
+	String nameEn;
+	String startDay;
+	String genre;
+	String nation;
+	String grade;
+	String time;
+	String summary;
+	double score;
+	int boxOffice;
+}
+class News
+{
+	int news_no;
+	String poster;
+	String title;
+	String journalist;
+	String content;
+	String regdate;
+}
+class Category
+{
+	int food_cate_no;
+	String poster;
+	String title;
+	String subject;
+}
+class Food
+{
+	int fno;
+	String name;
+	double score;
+	String address; // substring
+	String phone;
+	String type;
+	String price;
+	String parking;
+	String time;
+	String menu;
+}
+class Recipe
+{
+	int recipe_no;
+	String poster;
+	String title;
+	String chef;
+	int score;
+	int hit;
+	int showUser;
+}
+class Music
+{
+	int music_no;
+	int rank;
+	String state; // -, ▲, ▼
+	int idcrement;
+	String poster;
+	String title;
+	String singer;
+	String album;
+	int like;
+}
+class Shopping
+{
+	int goods_no; // 구분자
+	String discount;
+	String goods_poster;
+	String goods_name;
+	String goods_price;
+}
+class 연극
+{
+	int no;
+	String poster;
+	String title;
+	String genre;
+	String regdate;
+	String place;
+	String actor;
+	String grade;
+	String time;
+}
+public class MainClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

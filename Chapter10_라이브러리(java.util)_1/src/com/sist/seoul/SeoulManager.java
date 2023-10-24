c0ceb4f10251fe5b54d05419946d8b57657fdbf4
SIST <SIST@DESKTOP-5G69MRI>
2023-10-24 16:33:46 +0900

    2023-10-24

---
+++ Chapter10_라이브러리(java.util)_1/src/com/sist/seoul/SeoulManager.java
@@ -0,0 +1,61 @@
+package com.sist.seoul;
+
+import java.io.*;
+import java.util.*;
+
+public class SeoulManager {
+	public Seoul[] seoulAllData(int i)
+	{
+		Seoul[] seoul=null;
+		String path="c:\\javaDev\\";
+		if(i==1)
+			path+="seoul_location.txt";
+		else
+			path+="seoul_nature.txt";
+		
+		FileReader fr=null;
+		try
+		{
+			fr=new FileReader(path);
+			String data="";
+			int j=0;
+			while((j=fr.read())!=-1) // 파일 끝까지 읽어라
+			{
+				// read() => 한 글자 읽기 => 정수로 읽음
+				// A => 65
+				data+=(char)j;
+			}
+			
+			StringTokenizer st=new StringTokenizer(data, "\n");
+			seoul=new Seoul[st.countTokens()]; // 273, 110
+			
+			// 분리 => 데이터를 Seoul[]에 첨부
+			i=0;
+			
+			// 분리
+			String[] seouls=data.split("\n");
+			for(String s:seouls)
+			{
+				st=new StringTokenizer(s, "|");
+				seoul[i]=new Seoul();
+				// token(단어) => String st.nextToken()
+				seoul[i].setNo(Integer.parseInt(st.nextToken())); // 문자열을 정수형으로 변경
+				seoul[i].setName(st.nextToken());
+				seoul[i].setContent(st.nextToken());
+				seoul[i].setAddress(st.nextToken());
+				i++;
+			}
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}finally
+		{
+			try
+			{
+				fr.close();
+			}catch(Exception ex) {}
+		}
+		
+		return seoul;
+	}
+}

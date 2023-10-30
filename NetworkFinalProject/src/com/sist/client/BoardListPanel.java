19d4eac2d226565ef79e964352520a5284b6b44d
SIST <SIST@DESKTOP-5G69MRI>
2023-10-30 17:51:11 +0900

    2023-10-30

--- NetworkFinalProject/src/com/sist/client/BoardListPanel.java
+++ NetworkFinalProject/src/com/sist/client/BoardListPanel.java
@@ -1,12 +1,141 @@
 package com.sist.client;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-public class BoardListPanel extends JPanel{
-	public BoardListPanel()
+import javax.swing.table.*;
+import com.sist.vo.*;
+import com.sist.manager.*;
+import java.util.*;
+import java.text.*;
+
+public class BoardListPanel extends JPanel implements ActionListener{
+	JLabel la, pageLa;
+	JButton b1, b2, b3, b4;
+	JTable tabel;
+	DefaultTableModel model;
+	ControllPanel cp; // 화면 변경 ==> <jsp:include>
+	int curpage=1;
+	int totalPage=0;
+	BoardManager bm= new BoardManager();
+	public BoardListPanel(ControllPanel cp)
 	{
-		setBackground(Color.cyan);
+		this.cp=cp;
+		String[] col= {"번호", "제목", "이름", "작성일", "조회수"};
+		String[][] row=new String[0][5];
+		model=new DefaultTableModel(row, col)
+		{
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				// TODO Auto-generated method stub
+				return super.isCellEditable(row, column);
+			}
+			// 익명의 클래스 => 오버라이딩
+			// 편집 방지
+			
+		};
+		tabel=new JTable(model);
+		tabel.getTableHeader().setReorderingAllowed(false);
+		tabel.setShowVerticalLines(false);
+		tabel.setRowHeight(40);
+		
+		tabel.getColumn("번호").setPreferredWidth(30);
+		tabel.getColumn("제목").setPreferredWidth(400);
+		tabel.getColumn("이름").setPreferredWidth(100);
+		tabel.getColumn("작성일").setPreferredWidth(150);
+		tabel.getColumn("조회수").setPreferredWidth(50);
+		JScrollPane js=new JScrollPane(tabel);
+		
+		b1=new JButton("새글");
+		b2=new JButton("검색");
+		b3=new JButton("이전");
+		b4=new JButton("다음");
+		la=new JLabel("자유 게시판");
+		pageLa=new JLabel("1 page / 1 pages");
+		
+		// 배치
+		setLayout(null);
+		la.setHorizontalAlignment(JLabel.CENTER);
+		la.setFont(new Font("굴림체", Font.BOLD, 35));
+		la.setBounds(10, 15, 700, 50);
+		add(la);
+		
+		b1.setBounds(10, 75, 100, 30);
+		add(b1);
+		
+		js.setBounds(10, 115, 700, 450);
+		add(js);
+		
+		JPanel p=new JPanel();
+		p.add(b3);
+		p.add(pageLa);
+		p.add(b4);
+		
+		p.setBounds(10, 570, 710, 35);
+		add(p);
+		
+		// 이벤트
+		b1.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		boardList();
+	}
+	public void boardList()
+	{
+		// 전체를 지움
+		for(int i=model.getRowCount()-1;i>=0;i--)
+		{
+			model.removeRow(i);
+		}
+		ArrayList<BoardVO> list=bm.boardListData(curpage);
+		totalPage=bm.boardTotalPage();
+		pageLa.setText(curpage + " page / " + totalPage + " pages");
+		for(int i=list.size()-1;i>=0;i--)
+		{
+			BoardVO vo=list.get(i);
+			String[] data= {
+								String.valueOf(vo.getNo()),
+								vo.getSubject(),
+								vo.getName(),
+								new SimpleDateFormat("yyyy-MM-dd").format(vo.getRegdate()),
+								String.valueOf(vo.getHit())
+			};
+			
+			model.addRow(data);
+		}	
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b1) // b1을 클릭했다면
+		{
+			cp.bip.tf1.setText("");
+			cp.bip.tf2.setText("");
+			cp.bip.tf3.setText("");
+			cp.bip.ta.setText("");
+		  cp.card.show(cp, "insert");
+		}
+		else if(e.getSource()==b3)
+		{
+			if(curpage>1)
+			{
+				curpage--;
+				boardList();
+			}
+		}
+		else if(e.getSource()==b4)
+		{
+			if(curpage<totalPage)
+			{
+				curpage++;
+				boardList();
+			}
+		}
 	}
 }

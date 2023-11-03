0f5d4ed57cd9e993e6da2384ad295c46cc7636f1
SIST <SIST@DESKTOP-5G69MRI>
2023-11-03 12:18:28 +0900

    20230-11-03

--- NetworkFinalProject3/src/com/sist/server/Server3.java
+++ NetworkFinalProject3/src/com/sist/server/Server3.java
@@ -1,20 +1,21 @@
 package com.sist.server;
 /*
- *   서버
- *   ----
- *     접속 담당 ==> 1개만 사용
- *     통신 담당(요청 => 응답) ==> 클라이언트 접속자 수 만큼 
- *     ------ 스레드
- *     
- *     서버
- *      => 서버 구동 ServerSocket
- *                bind() ==> 개통(ip, port) => 유심
- *      => 대기(클라이언트 접근) => listen()
- *      => 접속 처리 => accept() => 발신자 정보 확인
- *         => 메모리에 저장
- *         => 스레드를 클라이언트마다 생성 => 따로 통신
- *         => 기능별(Function)
- *            스레드 => 사용자 요청 => 처리 결과 응답
+		서버
+		---
+		 접속 담당 ==> 1개만 사용
+		 통신 담당 (클라이언트 요청 => 응답) ==> 클라이언트 접속자 수만큼 사용
+		 ------- 쓰레드 이용
+		 
+		 서버
+		  => 서버 구동 ServerSocket
+		  			 bind() ==> 개통 (ip, port) => 유심
+		  => 대기 (클라이언트 접근) => listen()
+		  => 접속 처리 => accept() => 발신자 정보 확인
+		  	 => 메모리에 저장
+		  	 => 쓰레드를 클라이언트마다 생성 => 따로 통신이 가능
+		  	 => 기능별 (Function)
+		  	 	쓰레드 => 사용자 요청에 따라서 처리 결과 응답
+		  
  */
 import java.net.*;
 import java.io.*;
@@ -24,169 +25,168 @@ import com.sist.common.Function3;
 
 public class Server3 implements Runnable{
 	// 클라이언트 정보 저장 => id, name, sex, ip, port
-	private Vector<Client> waitVC=new Vector<Client>();
+	// 대기 소켓
+	private Vector<Client> waitVc=new Vector<Client>();
 	private ServerSocket ss;
-	private final int PORT=20000; // 10000으로 바꿀 것
+	private final int PORT=10000;
 	
-	// 서버 가동 ==> 한 번만 실행 가능
+	// 서버 가동 ==> 한번만 실행 가능하다. (같은 포트가 여러번 열리면 오류 발생)
 	public Server3()
 	{
 		try
 		{
-			ss=new ServerSocket(PORT); // IP자동 인식
+			ss=new ServerSocket(PORT); // IP 자동인식
 			// bind() => listen()
-			// 50명까지 한정
+			// 50명까지 접근인원이 한정되어 있다. (그 이상 되면 자동으로 서버가 종료된다.)
 			System.out.println("Server Start...");
+			
 		}catch(Exception ex) {}
 	}
 	
-	// 접속 시 처리
+	// 접속시 처리
 	public void run()
 	{
 		try
 		{
 			while(true)
 			{
-				// 클라이언트 정보 얻기 => Socket(ip, port)
-				Socket s=ss.accept(); // 발신자 정보를 받음
+				// 클라이언트 정보 얻기 => Socket (ip, port)
+				Socket s=ss.accept();
 				
 				Client client=new Client(s);
-				client.start(); // 통신 시작
-				
+				client.start(); // 통신 시작 부분
 			}
 		}catch(Exception ex) {}
 	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Server3 server=new Server3();
 		new Thread(server).start();
+
 	}
 	
-	// 통신 담당 => 따로 작동 => 스레드
+	// 통신 소켓
 	class Client extends Thread
 	{
-		String id, name, sex, pos;
+		String id, name, sex, pos; // pos: 위치
 		Socket s;
 		
-		// 클라이언트 요청/응답
-		// 본인 메모리에 써 놓고 서버가 읽어감 Socket에 써 놓음 메모리에 저장해 놓기 때문에 가져가도 파일 자체가 사라지지 않음
-		// 읽어가면 Refresh(사라짐)
-		BufferedReader in; // 요청 받은 경우
+		// 클라이언트 요청 및 응답
+		BufferedReader in; // 요청받은 경우
 		OutputStream out; // 응답
+		
 		public Client(Socket s)
 		{
 			this.s=s; // 클라이언트 컴퓨터
-			// 서버는 클라이언트 정보를 가지고 있어야 하고 클라이언트는 서버의 정보를 가지고 있어야 함
-			// PORT 연결선 => 서버는 고정 PORT / 클라이언트 자동 생서야 => 중복 없이 생성
+			// 서버는 클라이언트 정보 / 클라이언트는 서버에 대한 정보
+			// PORT 연결선 => 서버는 고정 PORT / 클라이언트 자동 생성 => 중복없이 생성
 			try
 			{
-				in=new BufferedReader(new InputStreamReader(s.getInputStream())); // 클라이언트의 소켓. 요청한값을 클라이언트 메모리에서 읽기
-				out=s.getOutputStream(); // 클라이언트에서 읽을 수 있게 메모리에 저장
+				in=new BufferedReader(new InputStreamReader(s.getInputStream())); // 클라이언트 메모리에서 요청한 값 읽어오기
+				out=s.getOutputStream(); // 읽은 값을 클라이언트 메모리에서 읽을 수 있게 저장
 			}catch(Exception ex) {}
 		}
-		// 요청 시 응답
-		public void run()
-		{
-			try
-			{
-				while(true)
+		
+		// 요청 => 응답 
+				public void run()
 				{
-					String msg=in.readLine();
-					System.out.println("Client => " + msg);
-					// 로그인 => 100|id|name|sex
-					StringTokenizer st=new StringTokenizer(msg, "|");
-					int protocol=Integer.parseInt(st.nextToken());
-					switch(protocol)
+					try
 					{
-						case Function3.LOGIN:
+						while(true)
 						{
-							id=st.nextToken();
-							name=st.nextToken();
-							sex=st.nextToken();
-							pos="대기실";
-							
-							// 접속되어 있는 사람들에게 전송
-							messageAll(Function3.LOGIN + "|" + id + "|"
-									   + name + "|" + sex + "|" + pos);
-							messageAll(Function3.WAITCHAT + "|[알림 ☞]"
-									    + name + "님 입장하셨습니다");
-							// waitVc => 저장
-							waitVC.add(this);
-							// Login창을 닫고, main을 보여줌
-							messageTo(Function3.MYLOG + "|" + id);
-							
-							// 접속자 명단을 보냄
-							for(Client client:waitVC)
+							String msg=in.readLine();
+							System.out.println("Client=>"+msg);
+							// 로그인 => 100|id|name|sex
+							StringTokenizer st=
+									new StringTokenizer(msg,"|");
+							int protocol=Integer.parseInt(st.nextToken());
+							switch(protocol)
 							{
-								messageTo(Function3.LOGIN + "|"
-										  + client.id + "|"
-										  + client.name + "|"
-										  + client.sex + "|"
-										  + client.pos);
+							  case Function3.LOGIN:
+							  {
+								id=st.nextToken();
+								name=st.nextToken();
+								sex=st.nextToken();
+								pos="대기실";
 								
-							}
-						}
-						break;
-						case Function3.WAITCHAT: // 웹 => jsp
-						{
-							String data=st.nextToken();
-							messageAll(Function3.WAITCHAT + "|[" + name + "]" + data);
-						}
-						break;
-						case Function3.EXIT:
-						{
-							messageAll(Function3.WAITCHAT + "|[알림 ☞]" + name + "님 퇴장하셨습니다");
-							messageAll(Function3.EXIT + "|" + id);
-							
-							// id를 테이블에서 제거
-							for(int i=0;i<waitVC.size();i++)
-							{
-								Client client=waitVC.get(i);
-								if(client.id.equals(id))
+								// 접속되어 있는 사람들에게 전송 
+								messageAll(Function3.LOGIN+"|"+id+"|"
+										+name+"|"+sex+"|"+pos);
+								messageAll(Function3.WAITCHAT+"|[알림 ☞] "+
+										name+"님 입장하셨습니다|red");
+								// waitVc => 저장 
+								waitVc.add(this);
+								// Login창을 닫고 , main을 보여준다 
+								messageTo(Function3.MYLOG+"|"+id);
+								// 접속자 명단을 보낸다 
+								for(Client client:waitVc)
 								{
-									messageTo(Function3.MYEXIT + "|");
-									waitVC.remove(i);
-									try
-									{
-										// 송수신 종료
-										in.close();
-										out.close();
-									}catch(Exception ex) {}
-									break;
+									messageTo(Function3.LOGIN+"|"
+											+client.id+"|"
+											+client.name+"|"
+											+client.sex+"|"
+											+client.pos);
 								}
+								// 개설된 방을 전송 ==> (X)
+							  }
+							  break;
+							  case Function3.WAITCHAT:// 웹 => jsp
+							  {
+								  String data=st.nextToken();
+								  String color=st.nextToken();
+								  messageAll(Function3.WAITCHAT+"|["+name+"]"+data+"|"+color);
+							  }
+							  break;
+							  case Function3.EXIT:
+							  {
+								  messageAll(Function3.WAITCHAT+"|[알림 ☞] "+name+"님 퇴장하셨습니다|red");
+								  messageAll(Function3.EXIT+"|"+id);
+								  // id를 테이블에서 제거
+								  for(int i=0;i<waitVc.size();i++)
+								  {
+									  Client client=waitVc.get(i);
+									  if(client.id.equals(id))
+									  {
+										  messageTo(Function3.MYEXIT+"|");
+										  waitVc.remove(i);
+										  try
+										  {
+											  //  송수신 종료
+											  in.close();
+											  out.close();
+										  }catch(Exception ex) {}
+										  break;
+									  }
+								  }
+							  }
+							  break;
 							}
+							
 						}
-						break;
-					}
+					}catch(Exception ex) {}
 				}
-			}catch(Exception ex) {}
-		}
-		
-		// 처리 방식 => 2개
-		// 1. 접속자 전체로 전송 => 로그인(먼저 로그인 한 사람)/나가기(남아 있는 사람)/채팅
-		public synchronized void messageAll(String msg)
-		{
-			// synchronized => 동기방식(스레드)
-			try
-			{
-				for(Client client:waitVC)
+				// 처리 방식 => 2개 
+				// 1. 접속자 전체로 전송 => 로그인(먼저 로그인 사람) / 나가기(남아 있는 사람) / 채팅 
+				public synchronized void messageAll(String msg)
 				{
-					client.messageAll(msg);
+					// synchronized => 동기방식 (쓰레드)
+					try
+					{
+						for(Client client:waitVc)
+						{
+							client.messageTo(msg);
+						}
+					}catch(Exception ex) {}
 				}
-			}catch(Exception ex) {}
+				// 2. 로그인 , 나가기 
+				public synchronized void messageTo(String msg)
+				{
+					try
+					{
+						out.write((msg+"\n").getBytes());
+					}catch(Exception ex) {}
+				}
+				
+			}
 		}
-		
-		// 2. 로그인, 나가기
-		public synchronized void messageTo(String msg)
-		{
-			try
-			{
-				out.write((msg + "\n").getBytes());
-			}catch(Exception ex) {}
-			
-			
-		}
-	}
-
-}
